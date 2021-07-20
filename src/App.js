14072d1894fbb62ab86fd253eb6c094d628815e7
DavidServias <davidservias@gmail.com>
2021-07-20 08:46:24 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import Sounds from "./Sounds"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+
+    return (
+      <div className="html-div">
+        <div className="body-div">
+          <div className="App" className="board">
+            <header className="App-header">
+            </header>
+            <Board />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
 }
+
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
+    this.Sounds = Sounds
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.state = {keyIsDown: false,
+                  sounds: [],
+                  disableKeys: []};
+    this.playAudio = this.playAudio.bind(this)
+
+  }
+
+  componentDidMount() {
+    console.log("mounted")
+    const arr = []
+    for (let i = 0; i < 9; i++){
+      let audio = new Audio;
+      audio.src = Sounds[i]['file']
+      arr.push(audio)
+    } 
+    this.setState({sounds:arr})
+  }
+
+  handleKeyPress(e) {
+    const index = this.keys.indexOf(e.key)
+    if (index !== -1 ) {
+      this.playAudio(index)
+    }
+    
+  }
+
+  playAudio(index){
+    let audio = this.state.sounds[index]
+    if (audio.paused === true) {
+      audio.play();
+    }
+    else if (audio.paused === false) {
+      audio.pause()
+      audio.currentTime = 0.0
+      audio.play();
+    }
+    
+  }
+
+  render() {
+    var ids = [];
+    var i;
+    for (i = 0; i < 9; i++) {
+      ids.push(i)
+    }
+    document.addEventListener('keydown', this.handleKeyPress, false);
+    document.addEventListener('keyup', this.handleKeyUp, false);
+
+    const btnGroup = ids.map((id) => (
+      <Button key={"id" + id}
+        audioSrc={this.Sounds[id]['file']}
+        soundName={this.Sounds[id]["name"]}
+        keyPressed={this.keys[id]}
+        id={id} />
+    ))
+    return (
+      <div>
+        <div className="board">
+          <h1 className="heading">David's Drum Machine</h1>
+          <p className="sub-heading">(It works, kind of!)</p>
+          {btnGroup}
+        </div>
+      </div>
+    )
+  }
+
+}
+
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.playSound = this.playSound.bind(this)
+    this.audio = new Audio;
+    this.audio.src = this.props.audioSrc
+  }
+  handleClick(audioEl) {
+    this.playSound()
+    
+  }
+  playSound() {
+    const audio = this.audio;
+    if (audio.paused === true) {
+      audio.play();
+    }
+    else if (audio.paused === false) {
+      audio.pause()
+      audio.currentTime = 0.0
+      audio.play();
+    }
+    
+  }
+  componentDidMount(){
+    console.log("in button, test: " + this.audio.src)
+  }
+  render() {
+    const id = this.props.id;   
+    const soundRef = this.soundRef
+    
+    return (
+      <div className="button-container">
+        
+        <button className={"button "}
+          type="button"
+          onClick={() => this.handleClick()}>{this.props.soundName}</button>
+
+      </div>
+    )
+  }
+
+}
+  
+
 
 export default App;

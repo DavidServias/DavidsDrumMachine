b41d6d9727b284d3a3751676ea2eaf2cb587bdb3
DavidServias <davidservias@gmail.com>
2021-07-21 21:12:10 -0700

    add display refactor to pass tests for FCC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import "./App.css";
-import Sounds from "./Sounds"
+// import Sounds from "./Sounds"
+
+const Sounds = [
+  { name: "Dsc_Oh", file: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  { name: "Kick_n_Hat", file: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
+  { name: "RP4_KICK_1", file: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
+  { name: "Chord_1", file: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" },
+  { name: "Chord_2", file: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3" },
+  { name: "Chord_3", file: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3" },
+  { name: "Give_us_a_light", file: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3" },
+  { name: "Dry_Ohh", file: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3" },
+  { name: "side_stick_1", file: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3 " }
+
+]
+
+
+
 
 
 class App extends React.Component {
@@ -12,7 +28,7 @@ class App extends React.Component {
     return (
       <div className="html-div">
         <div className="body-div">
-          <div className="App" className="board">
+          <div className="App" className="board" >
             <header className="App-header">
             </header>
             <Board />
@@ -28,38 +44,44 @@ class App extends React.Component {
 class Board extends React.Component {
   constructor(props) {
     super(props)
-    this.keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
+    this.keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
     this.Sounds = Sounds
     this.handleKeyPress = this.handleKeyPress.bind(this)
-    this.state = {keyIsDown: false,
-                  sounds: [],
-                  disableKeys: []};
+    this.state = {
+      keyIsDown: false,
+      sounds: [],
+      display: "",
+      playingIndex: -1
+    }
     this.playAudio = this.playAudio.bind(this)
+    this.updateDisplay = this.updateDisplay.bind(this)
 
   }
-
   componentDidMount() {
     console.log("mounted")
     const arr = []
-    for (let i = 0; i < 9; i++){
+    for (let i = 0; i < 9; i++) {
       let audio = new Audio;
       audio.src = Sounds[i]['file']
       arr.push(audio)
-    } 
-    this.setState({sounds:arr})
+    }
+    this.setState({ sounds: arr })
   }
-
+  updateDisplay(text) {
+    this.setState({ display: text })
+  }
   handleKeyPress(e) {
-    const index = this.keys.indexOf(e.key)
-    if (index !== -1 ) {
+    const index = this.keys.indexOf(e.key.toUpperCase())
+    if (index !== -1) {
       this.playAudio(index)
     }
-    
-  }
 
-  playAudio(index){
-    let audio = this.state.sounds[index]
+  }
+  playAudio(index) {
+    const audio = document.getElementById(this.keys[index])
+    console.log(audio)
     if (audio.paused === true) {
+      console.log("true")
       audio.play();
     }
     else if (audio.paused === false) {
@@ -67,9 +89,11 @@ class Board extends React.Component {
       audio.currentTime = 0.0
       audio.play();
     }
-    
-  }
+    this.updateDisplay(this.Sounds[index]['name'])
 
+
+
+  }
   render() {
     var ids = [];
     var i;
@@ -84,14 +108,18 @@ class Board extends React.Component {
         audioSrc={this.Sounds[id]['file']}
         soundName={this.Sounds[id]["name"]}
         keyPressed={this.keys[id]}
-        id={id} />
+        id={id}
+        playingIndex={this.state.playing}
+        updateDisplay={this.updateDisplay} />
     ))
     return (
-      <div>
-        <div className="board">
+      <div >
+        <div className="board" id="drum-machine">
           <h1 className="heading">David's Drum Machine</h1>
-          <p className="sub-heading">(It works, kind of!)</p>
           {btnGroup}
+          <h2 className="heading"
+            id="display">
+            {this.state.display}</h2>
         </div>
       </div>
     )
@@ -105,16 +133,32 @@ class Button extends React.Component {
     super(props)
     this.handleClick = this.handleClick.bind(this)
     this.playSound = this.playSound.bind(this)
-    this.audio = new Audio;
-    this.audio.src = this.props.audioSrc
+    this.keyTrigger = ""
+
   }
-  handleClick(audioEl) {
+  componentDidMount() {
+    const id = this.props.id
+    this.keyTrigger = (function () {
+      const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]
+      let index = id.toString().substring(id.length - 1);
+      let keyTrigger = keys[index]
+      return keyTrigger
+    })();
+
+  }
+
+
+  handleClick() {
     this.playSound()
-    
+    const button = document.getElementById(this.props.soundName)
+    console.log(button)
+
   }
   playSound() {
-    const audio = this.audio;
+    const audio = document.getElementById(this.keyTrigger)
+    console.log(audio)
     if (audio.paused === true) {
+      console.log("true")
       audio.play();
     }
     else if (audio.paused === false) {
@@ -122,28 +166,37 @@ class Button extends React.Component {
       audio.currentTime = 0.0
       audio.play();
     }
-    
-  }
-  componentDidMount(){
-    console.log("in button, test: " + this.audio.src)
+    this.props.updateDisplay(this.props.soundName)
+
   }
   render() {
-    const id = this.props.id;   
-    const soundRef = this.soundRef
-    
+    const id = this.props.id;
+    // const soundRef = this.soundRef
+    const keyTrigger = (function () {
+      const keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
+      let index = id.toString().substring(id.length - 1);
+      let keyTrigger = keys[index]
+      return keyTrigger.toUpperCase()
+    })();
+
     return (
-      <div className="button-container">
-        
+      <div className="button-container drum-pad"
+        id={this.props.soundName}
+        onClick={() => this.handleClick()}>
+        <audio
+          className="clip"
+          id={keyTrigger}
+          src={this.props.audioSrc}
+        ></audio>
         <button className={"button "}
-          type="button"
-          onClick={() => this.handleClick()}>{this.props.soundName}</button>
+          type="button">{keyTrigger}
+        </button>
 
       </div>
     )
   }
 
 }
-  
 
 
 export default App;
